011fd865f406245cc8737038e1b219d9f86c4b35
olivierdalang <olivier.dalang@gmail.com>
2020-02-17 15:14:59 +0100

    [plugin] adaptations

--- ckanext/inondationsdakar/plugin.py
+++ ckanext/inondationsdakar/plugin.py
@@ -6,38 +6,71 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as tk
 
 
-def create_country_codes():
-    '''Create country_codes vocab and tags, if they don't exist already.
+def create_document_types():
+    '''Create document_types vocab and tags, if they don't exist already.
 
     Note that you could also create the vocab and tags using CKAN's API,
     and once they are created you can edit them (e.g. to add and remove
     possible dataset country code values) using the API.
 
+    Voir les vocabulaire : https://127.0.0.1/api/3/action/vocabulary_list
+    Supprimer le vocabulaire (/!\ on perd assignation !)
+
     '''
     user = tk.get_action('get_site_user')({'ignore_auth': True}, {})
     context = {'user': user['name']}
     try:
-        data = {'id': 'country_codes'}
+        data = {'id': 'document_types'}
         tk.get_action('vocabulary_show')(context, data)
         logging.info("Example genre vocabulary already exists, skipping.")
     except tk.ObjectNotFound:
-        logging.info("Creating vocab 'country_codes'")
-        data = {'name': 'country_codes'}
+        # logging.info("Creating vocab 'document_types'")
+        data = {'name': 'document_types'}
         vocab = tk.get_action('vocabulary_create')(context, data)
-        for tag in (u'uk', u'ie', u'de', u'fr', u'es'):
-            logging.info(
-                    "Adding tag {0} to vocab 'country_codes'".format(tag))
+
+        # doc_types = [
+        #     "Plan de développement",
+        #     "Texte réglementaire",
+        #     "Loi",
+        #     "Article de presse",
+        #     "Rapport d'activités",
+        #     "Rapport final",
+        #     "Document diagnostic",
+        #     "Donnée météorologique",
+        #     "Donnée démographique",
+        #     "Donnée technique",
+        #     "Donnée spatiale",
+        #     "Carte",
+        #     "Thèse ou mémoire",
+        #     "Autre",
+        # ]
+        doc_types = [
+            u"projet de développement",
+            u"texte réglementaire et lois",
+            u"article de presse",
+            u"rapport de projet",
+            u"diagnostic",
+            u"données brutes",
+            u"carte",
+            u"thèse ou mémoire",
+            u"guide",
+            u"autre",
+        ]
+
+        for tag in doc_types:
+            # logging.info(
+            #         "Adding tag {0} to vocab 'document_types'".format(tag))
             data = {'name': tag, 'vocabulary_id': vocab['id']}
             tk.get_action('tag_create')(context, data)
 
 
-def country_codes():
+def document_types():
     '''Return the list of country codes from the country codes vocabulary.'''
-    create_country_codes()
+    create_document_types()
     try:
-        country_codes = tk.get_action('tag_list')(
-                data_dict={'vocabulary_id': 'country_codes'})
-        return country_codes
+        document_types = tk.get_action('tag_list')(
+                data_dict={'vocabulary_id': 'document_types'})
+        return document_types
     except tk.ObjectNotFound:
         return None
 
@@ -52,17 +85,8 @@ class InondationsDakarPlugin(plugins.SingletonPlugin,
     plugins.implements(plugins.IConfigurer, inherit=False)
     plugins.implements(plugins.IDatasetForm, inherit=False)
     plugins.implements(plugins.ITemplateHelpers, inherit=False)
+    plugins.implements(plugins.IFacets, inherit=False)
 
-    # These record how many times methods that this plugin's methods are
-    # called, for testing purposes.
-    num_times_new_template_called = 0
-    num_times_read_template_called = 0
-    num_times_edit_template_called = 0
-    num_times_search_template_called = 0
-    num_times_history_template_called = 0
-    num_times_package_form_called = 0
-    num_times_check_data_dict_called = 0
-    num_times_setup_template_variables_called = 0
 
     def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
@@ -70,7 +94,7 @@ class InondationsDakarPlugin(plugins.SingletonPlugin,
         tk.add_template_directory(config, 'templates')
 
     def get_helpers(self):
-        return {'country_codes': country_codes}
+        return {'document_types': document_types}
 
     def is_fallback(self):
         # Return True to register this plugin as the default handler for
@@ -83,20 +107,10 @@ class InondationsDakarPlugin(plugins.SingletonPlugin,
         return []
 
     def _modify_package_schema(self, schema):
-        # Add our custom country_code metadata field to the schema.
+        # Add our custom document_type metadata field to the schema.
         schema.update({
-                'country_code': [tk.get_validator('ignore_missing'),
-                    tk.get_converter('convert_to_tags')('country_codes')]
-                })
-        # Add our custom_test metadata field to the schema, this one will use
-        # convert_to_extras instead of convert_to_tags.
-        schema.update({
-                'custom_text': [tk.get_validator('ignore_missing'),
-                    tk.get_converter('convert_to_extras')]
-                })
-        # Add our custom_resource_text metadata field to the schema
-        schema['resources'].update({
-                'custom_resource_text' : [ tk.get_validator('ignore_missing') ]
+                'document_type': [tk.get_validator('ignore_missing'),
+                    tk.get_converter('convert_to_tags')('document_types')]
                 })
         return schema
 
@@ -117,60 +131,18 @@ class InondationsDakarPlugin(plugins.SingletonPlugin,
         # (e.g. on dataset pages, or on the search page)
         schema['tags']['__extras'].append(tk.get_converter('free_tags_only'))
 
-        # Add our custom country_code metadata field to the schema.
+        # Add our custom document_type metadata field to the schema.
         schema.update({
-            'country_code': [
-                tk.get_converter('convert_from_tags')('country_codes'),
+            'document_type': [
+                tk.get_converter('convert_from_tags')('document_types'),
                 tk.get_validator('ignore_missing')]
-            })
-
-        # Add our custom_text field to the dataset schema.
-        schema.update({
-            'custom_text': [tk.get_converter('convert_from_extras'),
-                tk.get_validator('ignore_missing')]
-            })
-
-        schema['resources'].update({
-                'custom_resource_text' : [ tk.get_validator('ignore_missing') ]
             })
         return schema
 
-    # These methods just record how many times they're called, for testing
-    # purposes.
-    # TODO: It might be better to test that custom templates returned by
-    # these methods are actually used, not just that the methods get
-    # called.
 
-    def setup_template_variables(self, context, data_dict):
-        InondationsDakarPlugin.num_times_setup_template_variables_called += 1
-        return super(InondationsDakarPlugin, self).setup_template_variables(
-                context, data_dict)
+    def dataset_facets(self, facets_dict, package_type):
 
-    def new_template(self):
-        InondationsDakarPlugin.num_times_new_template_called += 1
-        return super(InondationsDakarPlugin, self).new_template()
+        facets_dict['document_type'] = tk._('Document type')
 
-    def read_template(self):
-        InondationsDakarPlugin.num_times_read_template_called += 1
-        return super(InondationsDakarPlugin, self).read_template()
-
-    def edit_template(self):
-        InondationsDakarPlugin.num_times_edit_template_called += 1
-        return super(InondationsDakarPlugin, self).edit_template()
-
-    def search_template(self):
-        InondationsDakarPlugin.num_times_search_template_called += 1
-        return super(InondationsDakarPlugin, self).search_template()
-
-    def history_template(self):
-        InondationsDakarPlugin.num_times_history_template_called += 1
-        return super(InondationsDakarPlugin, self).history_template()
-
-    def package_form(self):
-        InondationsDakarPlugin.num_times_package_form_called += 1
-        return super(InondationsDakarPlugin, self).package_form()
-
-    # check_data_dict() is deprecated, this method is only here to test that
-    # legacy support for the deprecated method works.
-    def check_data_dict(self, data_dict, schema=None):
-        InondationsDakarPlugin.num_times_check_data_dict_called += 1
+        # Return the updated facet dict.
+        return facets_dict
